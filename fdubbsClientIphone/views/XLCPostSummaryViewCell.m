214e87918a290c7a1403375088551f630f97ad3d
Xu Lvcai <xulvcai2008@gmail.com>
2014-04-23 09:25:57 +0800

    clean up XLCPostSummaryViewCell.m

--- fdubbsClientIphone/views/XLCPostSummaryViewCell.m
+++ fdubbsClientIphone/views/XLCPostSummaryViewCell.m
@@ -35,51 +35,5 @@
     // Configure the view for the selected state
 }
 
-
-// 自绘分割线
-/*
-- (void)drawRect:(CGRect)rect
-{
-    
-    CGContextRef c = UIGraphicsGetCurrentContext();
-    CGContextSetStrokeColorWithColor(c, [[UIColor redColor] CGColor]);
-    CGContextSetLineWidth(c, 1);
-    
-    CGContextBeginPath(c);
-    CGContextMoveToPoint(c, 0.0f, rect.size.height-1/2);
-    CGContextAddLineToPoint(c, rect.size.width, rect.size.height-1/2);
-    CGContextStrokePath(c);
-}
- */
-/*
-- (void)drawRect:(CGRect)rect
-{
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    
-    CGContextSetFillColorWithColor(context, [UIColor clearColor].CGColor);
-    CGContextFillRect(context, rect);
-    
-    //上分割线，
-    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
-    CGContextStrokeRect(context, CGRectMake(5, -1, rect.size.width - 10, 10));
-    
-    //下分割线
-    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
-    CGContextStrokeRect(context, CGRectMake(5, rect.size.height, rect.size.width - 10, 10));
-}
-*/
-/*
-- (void)drawRect:(CGRect)rect
-{
-    NSLog(@"redraw");
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    
-    CGContextSetFillColorWithColor(context, [UIColor whiteColor].CGColor);
-    CGContextFillRect(context, rect);
-    
-    CGContextSetStrokeColorWithColor(context, [UIColor colorWithRed:0xE2/255.0f green:0xE2/255.0f blue:0xE2/255.0f alpha:1].CGColor);
-    CGContextStrokeRect(context, CGRectMake(0, rect.size.height - 1, rect.size.width, 1));
-}
-*/
 @end
 
