33c8352ec43a77dcac3f709e56c16bda4db409f0
Xu Lvcai <xulvcai2008@gmail.com>
2014-06-02 21:40:09 +0800

    add section

---
+++ fdubbsClientIphone/views/XLCCustomView/XLCSectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  XLCSectionView.h
+//  fdubbsClientIphone
+//
+//  Created by dennis on 14-6-2.
+//  Copyright (c) 2014年 cn.edu.fudan.ss.xulvcai.fdubbs.client. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XLCSectionViewCell : UITableViewCell
+
+@end
---
+++ fdubbsClientIphone/views/XLCCustomView/XLCSectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  XLCSectionView.m
+//  fdubbsClientIphone
+//
+//  Created by dennis on 14-6-2.
+//  Copyright (c) 2014年 cn.edu.fudan.ss.xulvcai.fdubbs.client. All rights reserved.
+//
+
+#import "XLCSectionViewCell.h"
+
+@implementation XLCSectionViewCell
+
+@end
