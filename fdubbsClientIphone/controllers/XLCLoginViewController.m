46042d1d8d9c7ed536c8a7c8760aac1ea80ecb49
Xu Lvcai <xulvcai2008@gmail.com>
2014-08-21 19:23:55 +0800

    add close button in login view

--- fdubbsClientIphone/controllers/XLCLoginViewController.m
+++ fdubbsClientIphone/controllers/XLCLoginViewController.m
@@ -11,6 +11,7 @@
 #import "XLCUserManager.h"
 #import "FRDLivelyButton.h"
 #import "XLCLoginResponse.h"
+#import "UIButton+Bootstrap.h"
 
 @interface XLCLoginViewController () <UITextFieldDelegate>
 
@@ -43,14 +44,26 @@
     // Remember to set the navigation bar to be NOT translucent
 	[self.navigationController.navigationBar setTranslucent:NO];
     
-    [self.navigationItem setHidesBackButton:YES animated:NO];
+    //[self.navigationItem setHidesBackButton:YES animated:NO];
+    FRDLivelyButton *leftBarButton = [[FRDLivelyButton alloc] initWithFrame:CGRectMake(0,0,22,18)];
+    [leftBarButton setOptions:@{ kFRDLivelyButtonLineWidth: @(2.0f),
+                                 kFRDLivelyButtonHighlightedColor: [UIColor colorWithRed:0.5 green:0.8 blue:1.0 alpha:1.0],
+                                 kFRDLivelyButtonColor: [UIColor whiteColor]
+                                 }];
+    [leftBarButton setStyle:kFRDLivelyButtonStyleClose animated:NO];
+    [leftBarButton addTarget:self action:@selector(backAction) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:leftBarButton];
+    [self addLeftBarButtonItem:leftBarButtonItem];
+    
     
     self.title = @"用户登录";
     self.titleColor = [UIColor whiteColor];
     
-    UIImage *bgImage = [[_loginButton backgroundImageForState:UIControlStateNormal]
-                        imageWithOverlayColor:[UIColor colorWithRed:66/255.0 green:139/255.0 blue:202/255.0 alpha:1]];
-    [_loginButton setBackgroundImage:bgImage forState:UIControlStateNormal];
+    //UIImage *bgImage = [[_loginButton backgroundImageForState:UIControlStateNormal]
+    //                    imageWithOverlayColor:[UIColor colorWithRed:66/255.0 green:139/255.0 blue:202/255.0 alpha:1]];
+    //[_loginButton setBackgroundImage:bgImage forState:UIControlStateNormal];
+    [_loginButton primaryStyle];
+    [_visitorMode infoStyle];
     
     [_userIdTextField setupTextFieldWithIconName:@"user_name_icon"];
     [_passwdTextField setupTextFieldWithIconName:@"password_icon"];
@@ -78,6 +91,33 @@
     
 }
 
+- (void)backAction
+{
+    for (UIViewController *parent = self.parentViewController; parent != nil;
+         parent = parent.parentViewController) {
+        NSLog(@"the class is %@", [[parent class] description]);
+        if ([parent isKindOfClass:[UITabBarController class]]) {
+            // jump to the tabbar
+            //[self presentModalViewController:parent animated:YES];
+            [(UITabBarController *)parent setSelectedIndex:0];
+            break;
+        }
+    }
+}
+
+- (void)addLeftBarButtonItem:(UIBarButtonItem *)leftBarButtonItem
+{
+    UIBarButtonItem *negativeSpacer = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0) {
+        // Add a negative spacer on iOS >= 7.0
+        negativeSpacer.width = -10;
+    } else {
+        // Just set the UIBarButtonItem as you would normally
+        negativeSpacer.width = 0;
+        [self.navigationItem setLeftBarButtonItem:leftBarButtonItem];
+    }
+    [self.navigationItem setLeftBarButtonItems:[NSArray arrayWithObjects:negativeSpacer, leftBarButtonItem, nil]];
+}
 
 - (void)didReceiveMemoryWarning
 {
@@ -131,8 +171,18 @@
     // Pass the selected object to the new view controller.
 }
 */
-- (IBAction)doVisitorBrowse:(id)sender {
-    
+- (IBAction)doVisitorBrowse:(id)sender
+{
+    for (UIViewController *parent = self.parentViewController; parent != nil;
+         parent = parent.parentViewController) {
+        NSLog(@"the class is %@", [[parent class] description]);
+        if ([parent isKindOfClass:[UITabBarController class]]) {
+            // jump to the tabbar
+            //[self presentModalViewController:parent animated:YES];
+            [(UITabBarController *)parent setSelectedIndex:0];
+            break;
+        }
+    }
 }
 
 -(IBAction)doUserLogin:(id)sender
